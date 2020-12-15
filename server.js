daa306890e40e3df3ce48fd67fb75d60069ca6e1
Mohan Kumar <mks10685@gmail.com>
2020-12-15 20:48:37 +0530

    fixed loading issue in react

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const morgan = require("morgan");
 const connectDB = require("./config/db");
 
 const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 dotenv.config();
 connectDB();
@@ -15,5 +19,8 @@ app.use("/api/v1/transaction", transaction);
 
 
 const PORT = process.env.PORT || 5000;
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
 
 app.listen(PORT, console.log(`listening on port ${PORT}`.yellow.bold));
